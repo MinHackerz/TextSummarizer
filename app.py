f15a452ea94026a3bedad54d1bff43c84eaeba60
Menajul Hoque <menajulhoque99@gmail.com>
2024-02-10 01:50:08 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request, jsonify
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lsa import LsaSummarizer
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/summarize', methods=['POST'])
+def summarize():
+    data = request.get_json()
+    text = data['text']
+
+    if not text:
+        return jsonify({'summary': 'Please enter some text to summarize.'})
+
+    summary = get_summary(text, num_sentences=3)  # Summarize within 50 words (approx. 3 sentences)
+    return jsonify({'summary': summary})
+
+def get_summary(text, num_sentences):
+    parser = PlaintextParser.from_string(text, Tokenizer('english'))
+    summarizer = LsaSummarizer()
+    summary = summarizer(parser.document, num_sentences)
+    return ' '.join(str(sentence) for sentence in summary)
+
+if __name__ == '__main__':
+    app.run(debug=True)
